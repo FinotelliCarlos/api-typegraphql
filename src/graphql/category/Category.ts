266fb28b4e2d3fcea003ac4dc5dd6df1075c4a95
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-12-17 09:57:46 -0300

    create categories

---
+++ src/graphql/category/Category.ts
@@ -0,0 +1,19 @@
+import { Field, ObjectType } from "type-graphql";
+
+
+
+@ObjectType()
+
+class Category {
+    @Field()
+    description: String;
+
+    @Field()
+    name: String;
+
+    @Field()
+    _id: String;
+
+}
+
+export default Category;
