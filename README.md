74633e710f33e11ca4263d4ac7c712854f2aba8e
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-12-17 09:03:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# api-typegraphql
+ API GraphQL em Node.js e TypeScript com TypeGraphQL
---
+++ src/utils/connection.ts
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://Iscodecluster:a4KQIwvAeT8gqKeF@cluster0.cf2km.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
